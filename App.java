7d4b6405e2c6ac9f685641b30bcb9554bc8e0416
SyncAdam <61192656+SyncAdam@users.noreply.github.com>
2023-04-01 11:38:46 +0200

    Add files via upload

--- App.java
+++ App.java
@@ -1,9 +1,21 @@
-import java.io.*;
+import java.io.IOException;
+
+import Bati.*;
+import Bati.Revetements.Revetement;
 
 public class App {
     public static void main(String[]args)
     {   
-        Niveau n0 = new Niveau(0, 3);
-        System.out.println(n0.appartements.get(0).pieces.get(0).surface());
+        Immeuble myImmeuble;
+        try
+        {
+            myImmeuble = Immeuble.loadImmeuble("FirstImmeuble.txt");
+            System.out.println(myImmeuble.niveau[0].appartements.get(0).pieces.get(0).pieceIntegre());
+            
+        }
+        catch(IOException | ClassNotFoundException e)
+        {
+        }
+        System.out.println(Revetement.readDef());
     }
 }
