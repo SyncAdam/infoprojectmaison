aca41c76f61021662372956c3b8d1ffe890bad08
SyncAdam <61192656+SyncAdam@users.noreply.github.com>
2023-05-15 17:26:00 +0200

    Add files via upload

---
+++ Batiment.java
@@ -0,0 +1,21 @@
+package insa.Batiment;
+abstract class Batiment {
+    
+    int idBatiment;
+    String typeBatiment;
+    int nbrNiveaux;
+
+public void afficher(){
+
+}
+public double surfaceRevetement (int idRevetement){
+    return(0);
+}
+public double devisBatiment (int idRevetement){
+    return 0;
+}
+
+public void dessiner(){
+    
+}
+} 
